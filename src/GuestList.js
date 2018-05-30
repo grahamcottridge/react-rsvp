0eb019fd89077355aab565e30155a949ff179faa
Graham Cottridge <codinggc@gmail.com>
2018-05-30 23:48:06 +0100

    Add ghost tile functionality for pending guest

--- src/GuestList.js
+++ src/GuestList.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Guest from './Guest';
+import PendingGuest from './PendingGuest';
 
 const GuestList = props =>
   <ul>
-    {props.guests
-      .filter(guest => !props.isFiltered || guest.isConfirmed)
-      .map((guest, index) =>
-       <Guest
-          key={index}
-          name={guest.name}
-          isConfirmed={guest.isConfirmed}
-          isEditing={guest.isEditing}
-          handleConfirmation={() => props.toggleConfirmationAt(index)}
-          handleToggleEditing={() => props.toggleEditingAt(index)}
-          setName={text => props.setNameAt(text, index)}
-          handleRemove={() => props.removeGuestAt(index)} />
-      )}
+    <PendingGuest name ={props.pendingGuest} />
+      {props.guests
+        .filter(guest => !props.isFiltered || guest.isConfirmed)
+        .map((guest, index) =>
+         <Guest
+            key={index}
+            name={guest.name}
+            isConfirmed={guest.isConfirmed}
+            isEditing={guest.isEditing}
+            handleConfirmation={() => props.toggleConfirmationAt(index)}
+            handleToggleEditing={() => props.toggleEditingAt(index)}
+            setName={text => props.setNameAt(text, index)}
+            handleRemove={() => props.removeGuestAt(index)} />
+        )}
+      }
   </ul>;
 
 GuestList.propTypes = {
@@ -25,7 +28,8 @@ GuestList.propTypes = {
   toggleEditingAt: PropTypes.func.isRequired,
   setNameAt: PropTypes.func.isRequired,
   isFiltered: PropTypes.bool.isRequired,
-  removeGuestAt: PropTypes.func.isRequired
+  removeGuestAt: PropTypes.func.isRequired,
+  pendingGuest: PropTypes.string.isRequired
 };
 
 export default GuestList;
